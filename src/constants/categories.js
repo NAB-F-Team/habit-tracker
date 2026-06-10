b39197617c104b8b08b9f3f787788c401821034a
Huong Giang Nguyen <gianghuong4949@gmail.com>
2026-06-10 22:55:27 +0700

    Add constants and habit mocked data

---
+++ src/constants/categories.js
@@ -0,0 +1,8 @@
+export const HABIT_CATEGORIES = [
+  "Health",
+  "Study",
+  "Work",
+  "Mindfulness",
+  "Fitness",
+  "Other"
+];
---
+++ src/constants/statuses.js
@@ -0,0 +1,18 @@
+export const HABIT_STATUSES = [
+  "Active",
+  "Paused",
+  "Archived"
+];
+
+export const CHECKIN_STATUSES = [
+  "Not Started",
+  "In Progress",
+  "Completed"
+];
+
+export const GOAL_TYPES = [
+  "Streak",
+  "Total Completions",
+  "Weekly Target",
+  "Monthly Target"
+];
