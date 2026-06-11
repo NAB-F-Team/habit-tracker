e300428b92da477d57dcb9f40e7ed135b49a4972
Ngan Pham <phamthanhngan04@gmail.com>
2026-06-11 16:36:47 +0700

    Add page stubs and update app routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,39 +2,23 @@ import { Routes, Route } from "react-router-dom";
 import HabitsPage from "./pages/HabitsPage";
 import DashboardPage from "./pages/DashboardPage";
 import SettingPage from "./pages/SettingPage";
-import Home from "./pages/Home";
+import DailyCheckinPage from "./pages/DailyCheckinPage";
 
 function App() {
-
   return (
     //Nhã: mấy component này chưa viết export function nên nó lỗi
     <Routes>
+      <Route path="/" element={<DailyCheckinPage />} />
 
-      <Route
-        path="/"
-        element={<Home />}
-      />
+      <Route path="/habits" element={<HabitsPage />} />
 
+      <Route path="/goals" element={<GoalsPage />} />
 
-      <Route
-        path="/habits"
-        element={<HabitsPage />}
-      />
+      <Route path="/dashboard" element={<DashboardPage />} />
 
-      <Route
-        path="/dashboard"
-        element={<DashboardPage />}
-      />
-
-      <Route
-        path="/settings"
-        element={<SettingPage />}
-      />
-
+      <Route path="/settings" element={<SettingPage />} />
     </Routes>
-
   );
-
 }
 
 export default App;
